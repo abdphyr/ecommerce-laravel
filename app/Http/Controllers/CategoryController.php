8c958659d9eebfaedf097c198ccdadeeb483f8fa
Abdumannon <abdphyr@gmail.com>
2023-01-07 21:37:00 +0500

    Work with image section add to CategoryController

--- app/Http/Controllers/CategoryController.php
+++ app/Http/Controllers/CategoryController.php
@@ -4,7 +4,9 @@ namespace App\Http\Controllers;
 
 use App\Http\Resources\CategoryResource;
 use App\Models\Category;
+use App\Models\Image;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Storage;
 use Illuminate\Support\Facades\Validator;
 
 class CategoryController extends Controller
@@ -35,6 +37,7 @@ class CategoryController extends Controller
     $validator = Validator::make($request->all(), [
       'name' => 'required|string|max:255',
       'info' => 'required|string',
+      'image' => 'nullable|image|mimes:jpeg,png,jpg,gif,svg|max:2048',
     ]);
     if ($validator->fails()) {
       return response()->json($validator->errors(), 400);
@@ -44,6 +47,15 @@ class CategoryController extends Controller
         'name' => $request->input('name'),
         'info' => $request->input('info')
       ]);
+      if ($image = $request->file('image')) {
+        $name = $image->getClientOriginalName();
+        $url = $image->storeAs('products', $name, 'public');
+        Image::create([
+          'url' => $url,
+          'imageable_id' => $category->id,
+          'imageable_type' => Category::class
+        ]);
+      }
     } catch (\Throwable $th) {
       return response()->json(['error' => $th->getMessage()], 500);
     }
@@ -55,6 +67,7 @@ class CategoryController extends Controller
     $validator = Validator::make($request->all(), [
       'name' => 'nullable|string|max:255',
       'info' => 'nullable|string',
+      'image' => 'nullable|image|mimes:jpeg,png,jpg,gif,svg|max:2048',
     ]);
     if ($validator->fails()) {
       return response()->json($validator->errors(), 400);
@@ -67,6 +80,17 @@ class CategoryController extends Controller
         'name' => $request->input('name') ?? $category->name,
         'info' => $request->input('info') ?? $category->info
       ]);
+      if ($image = $request->file('image')) {
+        Storage::disk('public')->delete($category->image->url);
+        $category->image()->delete();
+        $name = $image->getClientOriginalName();
+        $url = $image->storeAs('products', $name, 'public');
+        Image::create([
+          'url' => $url,
+          'imageable_id' => $category->id,
+          'imageable_type' => Category::class
+        ]);
+      }
     } catch (\Throwable $th) {
       return response()->json(['error' => $th->getMessage()], 500);
     }
@@ -79,6 +103,8 @@ class CategoryController extends Controller
       if (!$category = Category::find($id)) {
         return response()->json(['error' => 'Category not found'], 404);
       }
+      Storage::disk('public')->delete($category->image->url);
+      $category->image()->delete();
       $category->delete();
     } catch (\Throwable $th) {
       return response()->json(['error' => $th->getMessage()], 500);
