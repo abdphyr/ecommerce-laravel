ac05d70b26387f0f0877829ec02fd3af4271afcb
Abdumannon <abdphyr@gmail.com>
2023-01-15 12:59:57 +0500

    Add route recourses

--- routes/api.php
+++ routes/api.php
@@ -2,7 +2,9 @@
 
 use App\Http\Controllers\AuthController;
 use App\Http\Controllers\CategoryController;
+use App\Http\Controllers\CommentController;
 use App\Http\Controllers\ProductController;
+use App\Http\Controllers\TagController;
 use Illuminate\Support\Facades\Route;
 
 
@@ -10,10 +12,15 @@ use Illuminate\Support\Facades\Route;
 Route::post('register', [AuthController::class, 'register']);
 Route::post('login', [AuthController::class, 'login']);
 
-Route::middleware(['auth.jwt'])->group(function () {  
-    Route::post('refreshToken', [AuthController::class, 'refreshToken']);
-    Route::post('logout', [AuthController::class, 'logout']);
-    Route::get('user', [AuthController::class, 'user']);
-    Route::resource('category', CategoryController::class);
-    Route::resource('products', ProductController::class);
+Route::middleware(['auth.jwt'])->group(function () {
+  Route::post('refreshToken', [AuthController::class, 'refreshToken']);
+  Route::post('logout', [AuthController::class, 'logout']);
+  Route::get('user', [AuthController::class, 'user']);
 });
+
+Route::resources([
+  'categories' => CategoryController::class,
+  'products' => ProductController::class,
+  'comments' => CommentController::class,
+  'tags' => TagController::class
+]);
