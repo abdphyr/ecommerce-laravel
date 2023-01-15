0df2ddb3c8127f5a6894021b6c99ba15014517d1
Abdumannon <abdphyr@gmail.com>
2023-01-15 16:31:01 +0500

    Register RoleMiddleware in HttpKernel

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -3,6 +3,7 @@
 namespace App\Http;
 
 use App\Http\Middleware\JwtMiddleware;
+use App\Http\Middleware\RoleMiddleware;
 use Illuminate\Foundation\Http\Kernel as HttpKernel;
 
 class Kernel extends HttpKernel
@@ -65,6 +66,7 @@ class Kernel extends HttpKernel
         'signed' => \App\Http\Middleware\ValidateSignature::class,
         'throttle' => \Illuminate\Routing\Middleware\ThrottleRequests::class,
         'verified' => \Illuminate\Auth\Middleware\EnsureEmailIsVerified::class,
-        'auth.jwt' => JwtMiddleware::class
+        'auth.jwt' => JwtMiddleware::class,
+        'role' => RoleMiddleware::class
     ];
 }
